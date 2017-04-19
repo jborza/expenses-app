247daccce6138326fb3ce7db3b4a64cc507f288d
jborza <jborza@gmail.com>
2017-04-19 21:16:09 +0200

    worked out the syntax for grouping with lodash (expenses by category attempt #1)

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -18,18 +18,25 @@ export class DashboardComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.expenseService.getExpenses().then(data=>{
-            this.groupData(data);           
+        this.expenseService.getExpenses().then(data => {
+            this.groupData(data);
         }
         );
     }
 
-    groupData(data:Expense[]) {
+    groupData(data: Expense[]) {
         this.expenses = data;
-        this.expensesByCategory = _.groupBy(this.expenses, item=>item.category);
-        this.expensesByDay = _.groupBy(this.expenses, item => Math.floor(item.date.getTime() / (1000 * 60 * 60 / 24)));
+
+        this.expensesByCategory = _(this.expenses)
+            .groupBy(x => x.category)
+            .toPairs()
+            .map(p => ({ name: p[0], items:p[1] }))
+            .value();
+        //this.expensesByCategory = _.groupBy(this.expenses, item => item.category);
+
+        //this.expensesByDay = _.groupBy(this.expenses, item => Math.floor(item.date.getTime() / (1000 * 60 * 60 / 24)));
         console.log(this.expensesByCategory);
-        console.log(this.expensesByDay);
+        //console.log(this.expensesByDay);
     }
 
 }
